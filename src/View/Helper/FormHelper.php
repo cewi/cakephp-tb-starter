e3d3899ea2d639504c575520ac410332e01b038e
Cornelius Wichmann <c.wichmann@gmx.de>
2015-03-08 10:41:58 +0100

    change fileInput widget to jasny's version

--- src/View/Helper/FormHelper.php
+++ src/View/Helper/FormHelper.php
@@ -39,6 +39,32 @@ class FormHelper extends Helper
     use IconTrait;
 
     /**
+     * Construct the widgets and binds the default context providers.
+     * converts file-input widget in one that uses jasny's file-input-widget
+     * don't forget to include teh css and js!
+     *
+     * @param \Cake\View\View $View The View this helper is being attached to.
+     * @param array $config Configuration settings for the helper.
+     */
+    public function __construct(View $View, array $config = [])
+    {
+        $this->_defaultConfig['templates'] = array_merge($this->_defaultConfig['templates'], [
+            'file' => '<div class="fileinput fileinput-new input-group" data-provides="fileinput">
+                        <div class="form-control" data-trigger="fileinput">
+                            <i class="glyphicon glyphicon-file fileinput-exists"></i> <span class="fileinput-filename"></span>
+                        </div>
+                            <span class="input-group-addon btn btn-default btn-file">
+                                <span class="fileinput-new"><i class="glyphicon glyphicon-folder-open"></i>&nbsp;' . __('Select File') . '</span>
+                                <span class="fileinput-exists"><i class="glyphicon glyphicon-pencil"></i>&nbsp;' . __('Change') . '</span>
+                                    <input type="file" name="{{name}}">
+                            </span>
+                            <a href="#" class="input-group-addon btn btn-default fileinput-exists" data-dismiss="fileinput"><i class="glyphicon glyphicon-remove"></i>&nbsp;' . __('Remove') . '</a>
+                    </div>',
+        ]);
+        parent::__construct($View, $config);
+    }
+
+    /**
      * Converts "normal" postLinks in iconified postLinks
      * title is converted to an icon depending on action name
      * if $option keetTitle is set, icon will be added to teh original title.
@@ -72,7 +98,8 @@ class FormHelper extends Helper
      * @param array $options
      * @return string
      */
-    public function button($title, array $options = []){
+    public function button($title, array $options = [])
+    {
         if (isset($options['icon']) && !empty($options['icon'])) {
             $options['title'] = $title;
             $title = $this->_icon($options['icon']);
